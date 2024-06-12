7a3eeacc7dc0e30e9704940483475ed6e8925932
Xenia <andreeva19-91@mail.ru>
2024-06-13 00:30:31 +0300

    add styles for 768px, add swiper logic

--- index.js
+++ index.js
@@ -1,37 +1,14 @@
+let swiper;
 if (window.window.innerWidth < 768) {
-    const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-        spaceBetween: 16,
-        slidesPerView: 'auto',
-    
-        // If we need pagination
-        pagination: {
-        el: '.swiper-pagination',
-        enabled: true,
-        },
+   swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 16,
+    slidesPerView: 'auto',
+
+    pagination: {
+    el: '.swiper-pagination',
+    enabled: true,
+    },
     }); 
 };
-
-window.addEventListener('resize', function (evt) {
-    console.log(evt.target.innerWidth);
-    if (evt.target.innerWidth < 768) {
-       const swiper = new Swiper('.swiper', {
-            // Optional parameters
-            direction: 'horizontal',
-            loop: true,
-            spaceBetween: 16,
-            slidesPerView: 'auto',
-        
-            // If we need pagination
-            pagination: {
-            el: '.swiper-pagination',
-            enabled: true,
-            },
-        }); 
-    } 
-    if (evt.target.innerWidth >= 768) {
-        swiper.destroy(true, false);
-    }
-})

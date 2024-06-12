b2220af89f9d8299e8c1ebb6e8ba644f62d1f0b7
Xenia <andreeva19-91@mail.ru>
2024-06-12 15:38:35 +0300

    add some js-rules for swiper

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-});
+if (window.window.innerWidth < 768) {
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        spaceBetween: 16,
+        slidesPerView: 'auto',
+    
+        // If we need pagination
+        pagination: {
+        el: '.swiper-pagination',
+        enabled: true,
+        },
+    }); 
+};
 
+window.addEventListener('resize', function (evt) {
+    console.log(evt.target.innerWidth);
+    if (evt.target.innerWidth < 768) {
+       const swiper = new Swiper('.swiper', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            spaceBetween: 16,
+            slidesPerView: 'auto',
+        
+            // If we need pagination
+            pagination: {
+            el: '.swiper-pagination',
+            enabled: true,
+            },
+        }); 
+    } 
+    if (evt.target.innerWidth >= 768) {
+        swiper.destroy(true, false);
+    }
+})
